2c95cf36eb7bb156f8c13e15a411d8e98efba43f
thoan741 <42322751+thoan741@users.noreply.github.com>
2018-11-02 09:25:47 +0100

    Add files via upload

--- Laboration_8/main.py
+++ Laboration_8/main.py
@@ -1,11 +1,10 @@
 # Author: Thomas Andersson
-# Version: 1.2
-# Date: 2018-10-31
-# Kommentarer.
-
-
+# Version: 1.3
+# Date: 2018-11-02
+# Kommentarer: Felhantering gällande fel vid inläsning av filen morse.d
 
 from morse import *
+
 
 def main():
     """
@@ -13,21 +12,26 @@ def main():
     Alla andra jämförelser kollar om andra elementet som returnas ur konverteringsfunktionerna
     är sant eller falskt, där de respektive kommer ge falskt om det ej kan översättas.
     Kan det inte översättas åt något håll, kommer första stället i strängen där översättning inte fungerade
-    att printas ut för bägge konverteringsmetoderna.
+    att printas ut för respektive konverteringsmetod.
     """
     x = [True, True]
-    while x[1]:
-        x = inputManagement()
-        if not x[1]:
-            print(x[0])
-            print("Tack och adjö!")
-            break
-        elif convertSymbolsToMorse(x[0], symbolToMorseDict)[1]:
-            print(convertSymbolsToMorse(x[0], symbolToMorseDict)[0])
-        elif convertMorseToSymbols(x[0], morseToSymbolDict)[1]:
-            print(convertMorseToSymbols(x[0], morseToSymbolDict)[0])
-        else:
-            print((convertSymbolsToMorse(x[0], symbolToMorseDict)[0]), "kan inte översättas till morse!")
-            print((convertMorseToSymbols(x[0], morseToSymbolDict)[0]), "är inget morsetecken! ")
+    if symbolToMorseDict:
+        while x[1]:
+            x = inputManagement()
+            if not x[1]:
+                print(x[0])
+                print("Tack och adjö!")
+                break
+            elif convertSymbolsToMorse(x[0], symbolToMorseDict)[1]:
+                print(convertSymbolsToMorse(x[0], symbolToMorseDict)[0])
+            elif convertMorseToSymbols(x[0], morseToSymbolDict)[1]:
+                print(convertMorseToSymbols(x[0], morseToSymbolDict)[0])
+            else:
+                print((convertSymbolsToMorse(x[0], symbolToMorseDict)[0]), "kan inte översättas till morse!")
+                print((convertMorseToSymbols(x[0], morseToSymbolDict)[0]), "är inget morsetecken! ")
+    else:
+        print("Filen som hämtas in är inte på korrekt form, eller så har en fil som inte finns försökt hämtas in.")
+        print("Tack och adjö!")
+
 
 main()
--- Laboration_8/morse.py
+++ Laboration_8/morse.py
@@ -1,33 +1,36 @@
 # Author: Thomas Andersson
-# Date: 2018-10-31
-# Version 1.4
-# Lagt till morse.d som en fil som läses in.
+# Date: 2018-11-02
+# Version 1.6
+# Felhantering gällande felinläsning av filen morse.d.
 
-
-# myFile = open("morse.d.py")
-# improvedString = str(myFile.read())
-# improvedString = improvedString[:134] + 'Å' + \
-#                  improvedString[136:142] + 'Ä' + \
-#                  improvedString[144:149] + 'Ö' + \
-#                  improvedString[151:]
+try:
+    myFile = open("morse.d.py")
+    improvedString = str(myFile.read())
+    improvedString = improvedString[:134] + 'Å' + \
+                    improvedString[136:142] + 'Ä' + \
+                    improvedString[144:149] + 'Ö' + \
+                    improvedString[151:]
+except FileNotFoundError:
+    improvedString = "aa"
 """
 Jag öppnar filen morse.d i läsbart läge. Sedan läses filen in och konverteras till en teckensträng.
 Å, Ä och Ö läses inte in korrekt så de måste ändras innan jag fortsätter.
 """
+
 import urllib.request
 
 """
-urllib.request låter mig hämta text från hemsidor.
+urllib.request är ett bibliotek som låter mig bl.a. att läsa och spara text från hemsidor.
 """
 
-link = "http://www.nada.kth.se/kurser/su/DA2001/sudata18/laborationer/morse.d"
-f = urllib.request.urlopen(link)
-myFile = f.read()
-myString = str(myFile)[2:-1]
-improvedString = myString[0:134] + 'Å' + \
-                 myString[142:148] + 'Ä' + \
-                 myString[156:161] + 'Ö' + \
-                 myString[169:]
+# link = "http://www.nada.kth.se/kurser/su/DA2001/sudata18/laborationer/morse.d"
+# f = urllib.request.urlopen(link)
+# myFile = f.read()
+# myString = str(myFile)[2:-1]
+# improvedString = myString[0:134] + 'Å' + \
+#                  myString[142:148] + 'Ä' + \
+#                  myString[156:161] + 'Ö' + \
+#                  myString[169:]
 """
 Här snyggas textfilen som laddades ner till. Först konverteras den från bit till sträng.
 Sedan tar jag bort onödiga tecken i början av strängen. Bokstäver Å,Ä,Ö sparas inte korrekt, så jag byter ut de tecknen,
@@ -35,11 +38,11 @@ samt tar bort en flärp i slutet.
 """
 
 
-
 def makeDict(str):
     """
     makeDict skapar ett dictionary ur en teckensträng, där varje bokstav kommer att bli ett keyword
      och paras ihop med den morsesträng som tillhör ordet.
+    Try-delen kommer testa om strängen som läses in är på rätt format, annars skicka vidare false.
     :param str: str är en teckensträng där en bokstav kommer först, sedan ett nummer n,
      och sedan en morsekod som är n tecken långt
     :return: Ett dictionary där varje keyword är en bokstav/nummer/symbol som är parat ihop med
@@ -48,18 +51,23 @@ def makeDict(str):
     a = 0
     b = 1
     c = {}
-    while a < len(str):
-        c[str[a]] = str[a+2:int(str[b])+a+2]
-        a = int(str[b])+a+2
-        b = a+1
-    return c
+    try:
+        while a < len(str):
+            c[str[a]] = str[a+2:int(str[b])+a+2]
+            a = int(str[b])+a+2
+            b = a+1
+        return c
+    except ValueError:
+        return False
 
 
 symbolToMorseDict = makeDict(improvedString)
-# Jag skapar ett dictionary med funktionen makeDict och teckensträngen som laddades ned.
+# Jag skapar ett dictionary med funktionen makeDict och teckensträngen som hämtades in, efter att den korrigerats.
 # Denna kan användas för att konvertera symboler,bokstäver och nummer till morsekod.
-
-morseToSymbolDict = {v: k for k, v in symbolToMorseDict.items()}
+if symbolToMorseDict:
+    morseToSymbolDict = {v: k for k, v in symbolToMorseDict.items()}
+else:
+    pass
 # Här skapas ett nytt dictionary med hjälp av det som skapades över, där value och keyword har bytt plats överallt,
 # för att då få ett nytt dictionary som konverterar morsetecken till bokstäver,symboler eller siffror.
 
@@ -73,8 +81,6 @@ def inputManagement():
         return [a, True]
     except EOFError as e:
         return [e, False]
-
-
 
 
 def convertSymbolsToMorse(msg, aDict):
